1adfd70df19cef3b14700f762f687b2debdb80f0
Joshua Tan <jtanjoshua@gmail.com>
2020-07-23 20:20:30 +0800

    install and setup redux thunk

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import productsReducer from "./store/reducers/products";
 import cartReducer from "./store/reducers/cart";
 import orderReducer from "./store/reducers/orders";
 import ShopNavigator from "./navigation/ShopNavigator";
+import ReduxThunk from "redux-thunk";
 
 const rootReducer = combineReducers({
   products: productsReducer,
@@ -14,7 +15,7 @@ const rootReducer = combineReducers({
   order: orderReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 export default function App() {
   return (
